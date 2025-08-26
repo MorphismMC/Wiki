b4fdcfc3e6887ca823403bbb651a80dad89f2fe5
Magic_Sweepy <Magic_Sweepy@proton.me>
2025-08-26 19:05:42 +0800

    Only localized Markdown keys in zh.

--- docs/.vitepress/config/style.ts
+++ docs/.vitepress/config/style.ts
@@ -12,14 +12,6 @@ export const style = defineConfig({
       md.use(MermaidMarkdown)
     },
 
-    container: {
-      tipLabel: "提示",
-      warningLabel: "警告",
-      dangerLabel: "危险",
-      infoLabel: "信息",
-      detailsLabel: "详细信息"
-    }
-
   },
 
   vite: {
